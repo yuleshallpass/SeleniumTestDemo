8dcfdcaf8e2f0e632bcc2c493d2fd2c39fb473b3
yuleshallpass <143834147+yuleshallpass@users.noreply.github.com>
2024-03-14 06:04:27 +1100

    Add files via upload

--- SeleniumAutomationTest.java
+++ SeleniumAutomationTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import com.test.selenium.page.WDPage;
+
 
 public class SeleniumAutomationTest {
 	private WebDriver driver;
