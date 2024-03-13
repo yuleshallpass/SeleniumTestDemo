073c138d7142ac24a8cf0c57945a6c4fb7c05c4e
yuleshallpass <143834147+yuleshallpass@users.noreply.github.com>
2024-03-14 06:02:19 +1100

    Add files via upload

---
+++ SeleniumAutomationPage.java
@@ -0,0 +1,5 @@
+package com.test.selenium.page;
+
+public class SeleniumAutomationPage {
+
+}
---
+++ SeleniumAutomationTest.java
@@ -0,0 +1,39 @@
+package com.test.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumAutomationTest {
+	private WebDriver driver;
+    private WDPage wdPage;
+
+    @BeforeClass
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        driver = new ChromeDriver();
+        wdPage = new WDPage(driver);
+    }
+
+    @Test
+    public void testCountryFilter() {
+        wdPage.navigateToURL("https://viewpoint.glasslewis.com/WD/?siteId=DemoClient");
+        wdPage.selectCountryFilter("Belgium");
+        assertEquals(String wdPage, String "Belgium" );
+    }
+
+    @Test
+    public void testCompanyNameLink() {
+        wdPage.navigateToURL("https://viewpoint.glasslewis.com/WD/?siteId=DemoClient");
+        wdPage.clickCompanyNameLink("Activision Blizzard Inc");
+        assertEquals(String wdPage, String "Activision Blizzard Inc" );
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+}
